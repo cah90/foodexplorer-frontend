58401ed34dda2328d484d1acd853cba4c53b86e3
Cássia <cascrisbern@gmail.com>
2023-08-08 20:48:36 +0300

    initial commit

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles"
+
+export function SignIn() {
+	return (
+		<>
+			<Container>
+				<h1>Hello World!</h1>
+				<h1>Olá!</h1>
+			</Container>
+
+			<h1>Hola!</h1>
+		</>
+	)
+}
