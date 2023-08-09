50c76aff9cefdd79568814828864fd078dded602
Cássia <cascrisbern@gmail.com>
2023-08-09 20:25:13 +0300

    feat: added theme, global file included favicon

---
+++ src/styles/global.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  font-size: 62.5%;
+
+  --ff-poppins: "Poppins", sans-serif;
+  --ff-roboto: "Roboto", sans-serif;
+}
+
+body {
+  background-color: ${({ theme }) => theme.COLORS.DARK_400};
+  
+  font-size: 1.6rem;
+  font-family: var(--ff-poppins);
+
+  -webkit-font-smoothing: antialiased;
+}
+
+a {
+  text-decoration: none;
+}
+
+button, a {
+  cursor: pointer;
+  transition: filter 0.2s;
+}
+
+button:hover, a:hover {
+  filter: brightness(0.9);
+}
+
+
+
+
+
+
+`
