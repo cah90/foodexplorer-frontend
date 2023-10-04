bb1791a05978cf67a5dd01807b93b6cfe7d2a3af
Cássia <cascrisbern@gmail.com>
2023-10-04 17:05:33 +0300

    added: vite environment variable to switch between prod and dev environments

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-	// baseURL: "http://localhost:3333",
-	//baseURL: "https://rocketfoodexplorer-api.onrender.com",
 	baseURL: import.meta.env.BASE_API_URL || "http://localhost:3333",
 })
