9efd28a82afea393202a7b5ca351b99f62464b1f
Cássia <cascrisbern@gmail.com>
2023-10-04 16:50:55 +0300

    test

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,6 @@ import axios from "axios"
 
 export const api = axios.create({
 	// baseURL: "http://localhost:3333",
-	baseURL: "https://rocketfoodexplorer-api.onrender.com",
+	//baseURL: "https://rocketfoodexplorer-api.onrender.com",
+	baseURL: import.meta.env.BASE_API_URL || "http://localhost:3333",
 })
