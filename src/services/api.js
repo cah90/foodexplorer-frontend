f90f762321480d11952fe25a56ea1877dee451db
Cássia <cascrisbern@gmail.com>
2023-10-05 18:59:19 +0300

    fix on baseURL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-	baseURL: import.meta.env.BASE_API_URL || "http://localhost:3333",
+	//baseURL: "http://localhost:3333",
+	baseURL: "https://rocketfoodexplorer-api.onrender.com",
 })
