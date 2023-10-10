c5f866871fb2a3430d764d4342a4f90448e6ff75
Cássia <cascrisbern@gmail.com>
2023-10-10 17:24:53 +0300

    attempt to have the project running in development and production separately

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,7 @@ import axios from "axios"
 
 export const api = axios.create({
 	//baseURL: "http://localhost:3333",
-	baseURL: "https://rocketfoodexplorer-api.onrender.com",
+	//baseURL: "https://rocketfoodexplorer-api.onrender.com",
+
+	baseURL: import.meta.env.VITE_BASE_URL,
 })
